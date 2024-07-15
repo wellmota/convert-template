e992fd1229df402d3fadd44f37ee137d9a2001e3
wellmota <impeto.wellington@gmail.com>
2024-07-15 20:54:35 -0300

    chore: Add form submit event listener to capture form data

--- scripts.js
+++ scripts.js
@@ -1,4 +1,7 @@
+// Get form elements
+const form = document.querySelector("form")
 const amount = document.getElementById("amount")
+const currency = document.getElementById("currency")
 
 // Listen for input event on amount input to receive only numbers
 amount.addEventListener("input", () => {
@@ -6,8 +9,14 @@ amount.addEventListener("input", () => {
 
   // Remove all non-numeric characters
   const hasCharactersRegex = /\D+/g
-  
+
   amount.value = amount.value.replace(hasCharactersRegex, "")
-
-
 })
+
+// Listen for submit event on form
+form.onsubmit = (event) => {
+  event.preventDefault()
+
+  // Get form data
+  console.log(currency.value)
+}
